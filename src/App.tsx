6b436c33678acbb56186cc4466067439a24f61ac
foundway <fangwei.lee@gmail.com>
2025-04-26 12:44:11 -0700

    add xr (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import { useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
+import { XR, createXRStore } from '@react-three/xr'
 import { Button } from '@/components/ui/button'
-import Scene from './components/Scene'
 import { useAnimationStore } from './store/useAnimationStore'
 import { useModelStore } from './store/useModelStore'
-import { useRef } from 'react'
+import Scene from './components/Scene'
 
 const App = () => {
   const { setCurrentAnimation } = useAnimationStore()
   const { addModel } = useModelStore()
   const fileInputRef = useRef<HTMLInputElement>(null)
+  const store = createXRStore()
 
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
@@ -21,29 +23,32 @@ const App = () => {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas shadows camera={{ position: [3, 1, 3], fov: 50 }}>
-        {/* <Scene currentAnimation={currentAnimation} /> */}
-        <Scene />
+        <XR store={store}>
+          <Scene />
+        </XR>
       </Canvas>
       <div className="absolute top-8 left-8 flex flex-col gap-2">
         <input
           type="file"
           ref={fileInputRef}
-          accept=".gltf,.glb"
+          accept=".glb"
           onChange={handleFileUpload}
           className="hidden"
         />
         <Button onClick={() => fileInputRef.current?.click()}>
-          Upload GLTF Model
+          Upload GLTF
         </Button>
         <Button onClick={() => setCurrentAnimation('TRACK-idle')}>
           Normal Idle
         </Button>
-        {/* <Button onClick={() => setCurrentAnimation('TRACK-sad-idle')}> */}
         <Button onClick={() => setCurrentAnimation('TRACK-sad-idle')}>
           Sad Idle
         </Button>
         <Button onClick={() => setCurrentAnimation('TRACK-arm-stretching')}>
           Arm Stretching
+        </Button>
+        <Button onClick={() => store.enterVR()}>
+          Enter AR
         </Button>
       </div>
     </div>
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -5,29 +5,35 @@ import { Group } from 'three'
 import { Character } from './Character'
 import { useGLTF } from '@react-three/drei'
 import { useModelStore } from '../store/useModelStore'
+// import { useXR } from '@react-three/xr'
 
 const Scene = () => {
   const groupRef = useRef<Group>(null)
   const { models } = useModelStore()
   const lastModel = models[models.length - 1]
   const uploadedGLTF = lastModel ? useGLTF(lastModel) : null
+  // const { session } = useXR()
 
-  useFrame((_, delta) => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += delta * 0.5
-    }
-  })
+  // useFrame((_, delta) => {
+  //   // if (groupRef.current && !session) {
+  //   if (groupRef.current) {
+  //     groupRef.current.rotation.y += delta * 0.5
+  //   }
+  // })
 
   return (
     <>
-      <Environment
+      {/* <Environment
         files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/rostock_laage_airport_2k.hdr"
-        ground={{ height: 5, radius: 40, scale: 20 }} />
+        ground={{ height: 5, radius: 40, scale: 20 }} /> */}
+      <directionalLight position={[10, 10, 10]} intensity={1} />
+      <ambientLight intensity={1} />
       <group ref={groupRef}>
         <Character />
-        {lastModel && uploadedGLTF && <primitive object={uploadedGLTF.scene} />}
+        {/* {lastModel && uploadedGLTF && <primitive object={uploadedGLTF.scene} />} */}
       </group>
       <ContactShadows opacity={0.5} />
+      {/* {!session && <OrbitControls makeDefault target={[0, 1, 0]} />} */}
       <OrbitControls makeDefault target={[0, 1, 0]} />
     </>
   )
