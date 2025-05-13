2d7f1cffa20f9d572448f4797cc2337b7e647d04
foundway <fangwei.lee@gmail.com>
2025-05-12 22:28:02 -0700

    select menu placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,41 @@ import { useRef } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { XR, createXRStore } from '@react-three/xr'
 import { Button } from '@/components/ui/button'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 import Scene from './components/Scene'
 import { useModels, AppContextProvider } from './context/AppContext'
 import { BsHeadsetVr } from "react-icons/bs";
 import { MdOutlineFileUpload } from "react-icons/md";
+
+const ModelSelect = () => {
+  return (
+    <div className="absolute top-8 left-8">
+      <Select>
+        <SelectTrigger className="w-48 bg-black text-white rounded-full border-none p-6">
+          <SelectValue placeholder="Select a model" />
+        </SelectTrigger>
+        <SelectContent className="bg-black text-white border-none p-1">
+          <SelectGroup>
+            <SelectItem value="apple">Apple</SelectItem>
+            <SelectItem value="banana">Banana</SelectItem>
+            <SelectItem value="blueberry">Blueberry</SelectItem>
+            <SelectItem value="grapes">Grapes</SelectItem>
+            <SelectItem value="pineapple">Pineapple</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
 
 const UploadButton = () => {
   const fileInputRef = useRef<HTMLInputElement>(null)
@@ -27,11 +58,11 @@ const UploadButton = () => {
         className="hidden"
       />
       <Button 
-        className="absolute bottom-8 left-8 rounded-full gap-1 p-6"
+        className="absolute top-8 left-58 rounded-full gap-1 h-12"
         onClick={() => fileInputRef.current?.click()}
       >
         <MdOutlineFileUpload size={20} />
-        Load GLB
+        <p className="text-white text-xs">(.glb)</p>
       </Button>
     </>
   )
@@ -53,6 +84,7 @@ const App = () => {
             <Scene />
           </XR>
         </Canvas>
+        <ModelSelect />
         <UploadButton />
         <Button 
           className="absolute bottom-8 right-8 rounded-full gap-3 p-6" 
